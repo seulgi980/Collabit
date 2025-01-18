a19fbf52f668e5b9be9ff9bc329cea44093a3426
seon318 <jys7861@gmail.com>
2025-01-18 16:18:06 +0900

    Chore: 프로젝트 시작
    Related to: #60

---
+++ server/src/main/groovy/com/collabit/ServerApplication.groovy
@@ -0,0 +1,13 @@
+package com.collabit
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ServerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ServerApplication, args)
+    }
+
+}
