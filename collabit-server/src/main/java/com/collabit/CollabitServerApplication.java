4c8b0e0fde7c2f58949fca6fbf2835fea9d37132
seon318 <jys7861@gmail.com>
2025-01-20 19:45:10 +0900

    Chore: 백엔드 프로젝트 이름 변경
    Closes: S12P11A605-60

---
+++ collabit-server/src/main/java/com/collabit/CollabitServerApplication.java
@@ -0,0 +1,13 @@
+package com.collabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabitServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabitServerApplication.class, args);
+	}
+
+}
