3be9df72f867289ff5ce922636336ef44200f506
eunseon <jeongisline@gmail.com>
2025-01-22 17:42:38 +0900

    Feat: RedisService 클래스 작성 set, get, delete 메서드 정의

---
+++ collabit-server/src/main/java/com/collabit/global/service/RedisService.java
@@ -0,0 +1,23 @@
+package com.collabit.global.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+    private final StringRedisTemplate redisTemplate;
+
+    public void set(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public String get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+}
