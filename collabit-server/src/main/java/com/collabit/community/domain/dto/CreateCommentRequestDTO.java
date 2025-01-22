b015300a27779375347127775e3794c1a9d65172
jiseung <kimsz123456@gmail.com>
2025-01-22 17:34:00 +0900

    Feat: 댓글 CRUD 요청 및 응답 DTO 정의 CreateCommentRequest - CreateCommentResponse - GetCommentResponse - UpdateCommentResponse DTO 생성 Related to : #S12P11A605-137

---
+++ collabit-server/src/main/java/com/collabit/community/domain/dto/CreateCommentRequestDTO.java
@@ -0,0 +1,16 @@
+package com.collabit.community.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateCommentRequestDTO {
+    String content;
+    Integer parentCommentCode;
+}
