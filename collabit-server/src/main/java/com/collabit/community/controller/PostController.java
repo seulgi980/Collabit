3fcf3c7a307998f81a557ac42609dfd1420c9ac7
jiseung <kimsz123456@gmail.com>
2025-01-22 14:33:05 +0900

    Feat: 게시판 CRUD API 작성 PostController 구현 Related to: #S12P11A605-100

---
+++ collabit-server/src/main/java/com/collabit/community/controller/PostController.java
@@ -0,0 +1,58 @@
+package com.collabit.community.controller;
+
+import com.collabit.community.domain.dto.CreatePostRequestDTO;
+import com.collabit.community.domain.dto.CreatePostResponseDTO;
+import com.collabit.community.domain.dto.GetPostResponseDTO;
+import com.collabit.community.domain.dto.UpdatePostRequestDTO;
+import com.collabit.community.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "PostController", description = "게시판 API")
+@RestController
+@RequestMapping("/api/post")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @Operation(summary="게시글 등록",description = "게시글을 등록하는 API입니다.")
+    @PostMapping("/")
+    public ResponseEntity<?> createPost(@ModelAttribute CreatePostRequestDTO requestDTO){
+        CreatePostResponseDTO responseDTO = postService.createPost(requestDTO,"1");
+        return ResponseEntity.status(201).body(responseDTO);
+    }
+
+    @Operation(summary="게시글 목록 조회",description = "게시글 목록을 조회하는 API입니다.")
+    @GetMapping("/")
+    public ResponseEntity<?> getPostList(){
+        List<GetPostResponseDTO> list = postService.getPostList("1");
+        return ResponseEntity.status(200).body(list);
+    }
+
+    @Operation(summary="게시글 상세 조회",description = "게시글을 상세 조회하는 API입니다.")
+    @GetMapping("/{postCode}")
+    public ResponseEntity<?> getPost(@PathVariable int postCode){
+        GetPostResponseDTO responseDTO = postService.getPost(postCode);
+        return ResponseEntity.status(200).body(responseDTO);
+    }
+
+    @Operation(summary="게시글 수정",description = "게시글을 수정하는 API입니다.")
+    @PutMapping("/{postCode}")
+    public ResponseEntity<?> updatePost(@PathVariable int postCode,@RequestBody UpdatePostRequestDTO requestDTO){
+        GetPostResponseDTO responseDTO = postService.updatePost(postCode, requestDTO);
+        return ResponseEntity.status(201).body(responseDTO);
+    }
+
+    @Operation(summary="게시글 삭제",description = "게시글을 삭제하는 API입니다.")
+    @DeleteMapping("/{postCode}")
+    public ResponseEntity<?> deletePost(@PathVariable int postCode){
+        postService.deletePost(postCode);
+        return ResponseEntity.status(204).build();
+    }
+}
--- collabit-server/src/main/java/com/collabit/post/controller/PostController.java
+++
@@ -1,5 +0,0 @@
-package com.collabit.post.controller;
-
-class post {
-    
-}
