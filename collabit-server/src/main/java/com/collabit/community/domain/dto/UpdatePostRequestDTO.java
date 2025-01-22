961aaadcce4ad24ad81eccabc450ca59b8e605ca
jiseung <kimsz123456@gmail.com>
2025-01-22 14:39:04 +0900

    Feat: 게시판 CRUD 요청 및 응답 DTO 정의 CreatePostRequest - CreatedPostResponse - GetPostResponse - UpdatePostRequest DTO 생성 Related to : #S12P11A605-98

---
+++ collabit-server/src/main/java/com/collabit/community/domain/dto/UpdatePostRequestDTO.java
@@ -0,0 +1,15 @@
+package com.collabit.community.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatePostRequestDTO {
+    String content;
+    String[] images;
+}
