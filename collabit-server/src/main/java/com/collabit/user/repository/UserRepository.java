60ca38b7c712a9193fa931c873b4e93c3f91fefa
lgh9776 <lgh9776@naver.com>
2025-01-23 17:23:39 +0900

    Feat: OAuth2Service 연동 비즈니스 로직 추가
    연동을 위한 유저 코드 임시 저장
    GitHub 계정 연동을 위한 linkGithubAccount 메소드 구현
    중복 연동 체크 로직 추가
    성공 핸들러에서 프론트에 어떤 요청에 대한 성공인지 알려주기 위한 인증 상태 설정
    
    Resolves: S12P11A605-155

--- collabit-server/src/main/java/com/collabit/user/repository/UserRepository.java
+++ collabit-server/src/main/java/com/collabit/user/repository/UserRepository.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
+    Optional<User> findByCode(String userCode);
+
     // GitHub 아이디로 사용자 찾기
     Optional<User> findByGithubId(String githubId);
 
+    // 해당 GitHub 아이디가 이미 존재하는지 확인
+    boolean existsByGithubId(String githubId);
 }
