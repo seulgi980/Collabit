1d0f1af22ec78d460f680e239e2a301146e0d0d7
clapsheep <sktndid1203@naver.com>
2025-01-23 15:54:01 +0900

    feat : 메인페이지 개발 - 모바일 nav 개발 - 데스크탑, 모바일 header 개발 - footer 개발 closes #S12P11A605-205, #S12P11A605-206, #S12P11A605-209,  #S12P11A605-211

--- collabit-client/src/app/(routes)/signup/page.tsx
+++ collabit-client/src/app/(routes)/signup/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="flex w-full flex-col items-center justify-center gap-10 py-20">
       <h2 className="text-4xl font-bold">회원가입</h2>
-      <Progress className="bg-gray-200" value={25} />
+      <Progress className="bg-gray-200" value={20} />
     </div>
   );
 }
