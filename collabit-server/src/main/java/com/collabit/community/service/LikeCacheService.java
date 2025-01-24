3e45b7518434a3319aef2f6adcb229cd75a2d430
jiseung <kimsz123456@gmail.com>
2025-01-24 12:24:41 +0900

    Style: LikeCacheService 파일이름 변경 PostCacheService -> LikeCacheService 변경 Related to : #S12P11A605-77

--- collabit-server/src/main/java/com/collabit/community/service/LikeCacheService.java
+++ collabit-server/src/main/java/com/collabit/community/service/LikeCacheService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class PostCacheService {
+public class LikeCacheService {
 
     private final PostLikeRepository postLikeRepository;
 
