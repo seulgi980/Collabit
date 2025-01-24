86f6c5ece94edf31a7839fdf6609ac70d5cdacc9
seulgi980 <seulgi980@naver.com>
2025-01-24 16:57:06 +0900

    Feat: swagger용 @Schema 태그 추가

--- collabit-server/src/main/java/com/collabit/auth/domain/dto/UserSignupRequestDto.java
+++ collabit-server/src/main/java/com/collabit/auth/domain/dto/UserSignupRequestDto.java
@@ -1,5 +1,6 @@
 package com.collabit.auth.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -10,15 +11,18 @@ import lombok.Getter;
 @AllArgsConstructor
 public class UserSignupRequestDto {
 
+    @Schema(description = "사용자의 이메일", example = "user@example.com", required = true)
     @NotBlank(message = "이메일을 입력해주세요")
     @Email(message = "올바른 이메일 형식이 아닙니다")
     private final String email;
 
+    @Schema(description = "사용자의 닉네임", example = "nickname", required = true)
     @NotBlank(message = "닉네임을 입력해주세요")
     @Pattern(regexp = "^.{2,16}$",
     message = "닉네임은 2자 이상 16자 이하로 입력해주세요")
     private final String nickname;
 
+    @Schema(description = "사용자의 비밀번호사용자의 비밀번호 영문, 숫자, 특수문자를 포함하여 8~16자", example = "password1234", required = true)
     @NotBlank(message = "비밀번호를 입력해주세요")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$",
             message = "비밀번호는 영문, 숫자, 특수문자를 포함하여 8~16자여야 합니다")
