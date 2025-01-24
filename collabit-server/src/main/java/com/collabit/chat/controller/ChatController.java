17b69f2d590c4cf8f9a3ecbc240b453dddcb82e3
eunseon <jeongisline@gmail.com>
2025-01-24 16:07:22 +0900

    Refactor: ChatController에 user 더미 데이터로 수정

--- collabit-server/src/main/java/com/collabit/chat/controller/ChatController.java
+++ collabit-server/src/main/java/com/collabit/chat/controller/ChatController.java
@@ -5,7 +5,7 @@ import com.collabit.chat.service.ChatRoomDetailService;
 import com.collabit.chat.service.ChatRoomListService;
 import com.collabit.chat.service.ChatService;
 import com.collabit.global.common.PageResponseDTO;
-import com.collabit.global.security.securityUtil;
+import com.collabit.global.security.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,18 +20,18 @@ public class ChatController {
     private final ChatService chatService;
     private final ChatRoomListService chatRoomListService;
     private final ChatRoomDetailService chatRoomDetailService;
-    private final SecurityUtil securityUtil;
 
     @PostMapping("/room")
     public ResponseEntity<?> getOrCreateChatRoom(@RequestBody ChatRoomRequestDTO requestDTO, @RequestHeader("Authorization") String token) {
-        String userCode = securityUtil.getCurrentUserId(token);
+        String userCode = SecurityUtil.getCurrentUserId();
         ChatRoomResponseDTO responseDTO = chatRoomListService.saveChatRoom(userCode, requestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
     @GetMapping("/room")
     public ResponseEntity<?> getChatRoomList(@RequestBody ChatRoomListRequestDTO requestDTO, @RequestHeader("Authorization") String token) {
-        String userCode = securityUtil.getCurrentUserId(token);
+//      String userCode = securityUtil.getCurrentUserId(token);
+        String userCode = "1";
         PageResponseDTO responseDTO = chatRoomListService.getChatRoomList(userCode, requestDTO);
         if (((List<?>) responseDTO.getContent()).isEmpty()) return ResponseEntity.noContent().build();
         return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
@@ -39,7 +39,8 @@ public class ChatController {
 
     @GetMapping("/room/{roomCode}")
     public ResponseEntity<?> getChatMessages(@RequestBody ChatRoomDetailRequestDTO requestDTO, @RequestHeader("Authorization") String token, @PathVariable int roomCode) {
-        String userCode = securityUtil.getCurrentUserId(token);
+//      String userCode = securityUtil.getCurrentUserId(token);
+        String userCode = "1";
         PageResponseDTO responseDTO = chatRoomDetailService.getChatRoomDetail(userCode, requestDTO);
         if (((ChatRoomDetailResponseDTO) responseDTO.getContent()).getMessages().isEmpty())
             return ResponseEntity.noContent().build();
@@ -48,14 +49,16 @@ public class ChatController {
 
     @GetMapping("/unread")
     public ResponseEntity<?> getUnreadMessages(@RequestHeader("Authorization") String token) {
-        String userCode = securityUtil.getCurrentUserId(token);
+//      String userCode = securityUtil.getCurrentUserId(token);
+        String userCode = "1";
         ChatUnreadResponseDTO responseDTO = chatRoomListService.getUnreadMessagesForUser(userCode);
         return ResponseEntity.ok(responseDTO);
     }
 
     @GetMapping("/{nickname}")
     public ResponseEntity<?> getChatRoomWithNickname(@RequestHeader("Authorization") String token, @RequestParam String nickname) {
-        String userCode = securityUtil.getCurrentUserId(token);
+//      String userCode = securityUtil.getCurrentUserId(token);
+        String userCode = "1";
         ChatRoomResponseDTO responseDTO = chatRoomListService.getChatRoomByNickname(userCode, nickname);
         if (responseDTO.getRoomCode() == 0) return ResponseEntity.noContent().build();
         return ResponseEntity.ok(responseDTO);
