f5f1f945031c80949583273c10fc06cddfbecc98
lgh9776 <lgh9776@naver.com>
2025-01-25 08:53:17 +0900

    Feat: OAuth2Controller 수정
    link 요청 시 usercode 검증만 하도록 수정 (임시저장 로직 삭제)
    임시저장을 하지 않기 때문에 OAuth2Service 주입받지 않음
    redirect 로직이 중복되어 메소드로 분리.
    로그인/회원가입 부분에서 usercode가 존재할 시 예외처리
    
    Related to: S12P11A605-225

--- collabit-server/src/main/java/com/collabit/oauth/controller/OAuth2Controller.java
+++ collabit-server/src/main/java/com/collabit/oauth/controller/OAuth2Controller.java
@@ -1,7 +1,6 @@
 package com.collabit.oauth.controller;
 
 import com.collabit.global.security.SecurityUtil;
-import com.collabit.oauth.service.OAuth2Service;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,18 +18,16 @@ import java.net.URI;
 @RestController
 public class OAuth2Controller {
 
-    private final OAuth2Service oAuth2Service;
-
-    public OAuth2Controller(OAuth2Service oAuth2Service){
-        this.oAuth2Service = oAuth2Service;
-    }
-
     @Operation(summary = "깃허브 로그인, 회원가입")
     @GetMapping
     public ResponseEntity<String> getGithubAccount(){
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(URI.create("/oauth2/authorization/github"));
-        return new ResponseEntity<>(headers, HttpStatus.FOUND);
+        String userCode = SecurityUtil.getCurrentUserId();
+
+        if(userCode != null){
+            throw new RuntimeException("로그인 정보가 있습니다. GitHub 연동을 해주세요.");
+        }
+
+        return redirectToGithub();
     }
 
     @Operation(summary = "일반 회원의 깃허브 연동 요청")
@@ -38,14 +35,17 @@ public class OAuth2Controller {
     public ResponseEntity<String> linkGithub(HttpServletRequest request) {
         String userCode = SecurityUtil.getCurrentUserId();
 
-        if(userCode != null){
-            oAuth2Service.storeUserCodeForLinking(userCode);
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.setLocation(URI.create("/oauth2/authorization/github"));
-            return new ResponseEntity<>(headers, HttpStatus.FOUND);
+        if(userCode == null){
+            throw new RuntimeException("토큰 정보가 유효하지 않습니다.");
         }
 
-        throw new RuntimeException("토큰 정보가 유효하지 않습니다.");
+        return redirectToGithub();
+    }
+
+    // 사용자 동의를 위한 GitHub 페이지로 redirect 하는 메소드 
+    private ResponseEntity<String> redirectToGithub() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/oauth2/authorization/github"));
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 }
