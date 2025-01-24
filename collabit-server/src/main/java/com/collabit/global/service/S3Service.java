7595d3e5a89d317d1691ffcd8b31d0c1f2cb8971
jiseung <kimsz123456@gmail.com>
2025-01-24 14:59:32 +0900

    Fix: S3Service 수정 주석 추가 및 로직 수정 Related to: #S12P11A605-217

--- collabit-server/src/main/java/com/collabit/global/service/S3Service.java
+++ collabit-server/src/main/java/com/collabit/global/service/S3Service.java
@@ -12,10 +12,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class S3Service {
@@ -36,27 +38,24 @@ public class S3Service {
             metadata.setContentLength(file.getSize());
             amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, metadata));
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
         }
         return extractUrl(amazonS3.getUrl(bucket, fileName).toString());
     }
 
     public boolean delete(String fileUrl) {
         try {
-            amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileUrl));
+            amazonS3.deleteObject(new DeleteObjectRequest(bucket, BASEURL + fileUrl));
             return true;
         } catch (AmazonServiceException e) {
-            System.err.println("Error occurred while deleting object from S3: " + e.getMessage());
+            log.error(e.getMessage());
         } catch (SdkClientException e) {
-            System.err.println("Client error while deleting object from S3: " + e.getMessage());
+            log.error(e.getMessage());
         }
         return false;
     }
 
     private String extractUrl(String Url) {
-        if (Url.startsWith(BASEURL)) {
-            return Url.substring(BASEURL.length());
-        }
-        return null;
+        return Url.substring(BASEURL.length());
     }
 }
