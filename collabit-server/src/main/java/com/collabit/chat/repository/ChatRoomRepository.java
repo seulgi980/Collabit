17546c7872fd5f4ac65f3013932bed88812c7671
eunseon <jeongisline@gmail.com>
2025-01-24 15:38:02 +0900

    Feat: 유저가 속한 모든 채팅방 확인 기능 추가 Related to: S12P11A605-118

--- collabit-server/src/main/java/com/collabit/chat/repository/ChatRoomRepository.java
+++ collabit-server/src/main/java/com/collabit/chat/repository/ChatRoomRepository.java
@@ -1,6 +1,8 @@
 package com.collabit.chat.repository;
 
 import com.collabit.chat.domain.entity.ChatRoom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,9 @@ import java.util.Optional;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Integer> {
+    //유니크 코드로 채팅방 찾기
     Optional<ChatRoom> findByUniqueCode(String uniqueCode);
+
+    //유저가 속한 모든 채팅방 확인
+    Page<ChatRoom> findByUserCode1OrUserCode2(String userCode1, String userCode2, Pageable pageable);
 }
