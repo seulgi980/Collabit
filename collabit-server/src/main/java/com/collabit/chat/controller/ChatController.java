2b92119aa3c06b8134fb8146bb1cc5a0a7009994
eunseon <jeongisline@gmail.com>
2025-01-24 15:29:19 +0900

    Feat: ChatController 기능 추가 채팅방 생성, 리스트 조회, 메시지 조회 기능 읽지 않은 메시지 존재 여부 확인 기능 닉네임으로 채팅방 존재 여부 확인 기능 Related to: S12P11A605-112, S12P11A605-120, S12P11A605-224

--- collabit-server/src/main/java/com/collabit/chat/controller/ChatController.java
+++ collabit-server/src/main/java/com/collabit/chat/controller/ChatController.java
@@ -1,51 +1,63 @@
 package com.collabit.chat.controller;
 
-import com.collabit.chat.domain.dto.ChatMessagePubDTO;
-import com.collabit.chat.domain.dto.ChatMessageSubDTO;
-import com.collabit.chat.domain.dto.ChatRoomRequestDTO;
-import com.collabit.chat.domain.dto.ChatRoomResponseDTO;
-import com.collabit.chat.service.ChatMessageService;
-import com.collabit.chat.service.ChatRoomService;
+import com.collabit.chat.domain.dto.*;
+import com.collabit.chat.service.ChatRoomDetailService;
+import com.collabit.chat.service.ChatRoomListService;
 import com.collabit.chat.service.ChatService;
+import com.collabit.global.common.PageResponseDTO;
+import com.collabit.global.security.securityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@Controller
+@RestController("/api/chat")
 public class ChatController {
 
     private final ChatService chatService;
-    private final ChatRoomService chatRoomService;
-    private final ChatMessageService chatMessageService;
+    private final ChatRoomListService chatRoomListService;
+    private final ChatRoomDetailService chatRoomDetailService;
+    private final SecurityUtil securityUtil;
 
-    @MessageMapping("/chatMessage")
-    public void receiveMessage(ChatMessageSubDTO chatMessageSubDTO) {
-        chatService.processMessage(chatMessageSubDTO);
-    }
-
-    @PostMapping("/api/chat/rooms")
-    public ResponseEntity<?> createChatRoom(@RequestHeader("Authorization") String token, ChatRoomRequestDTO requestDTO) {
-        //token에서 userCode 추출
-        String userCode = "1";
-        ChatRoomResponseDTO responseDTO = chatRoomService.saveChatRoom(userCode, requestDTO);
+    @PostMapping("/room")
+    public ResponseEntity<?> getOrCreateChatRoom(@RequestBody ChatRoomRequestDTO requestDTO, @RequestHeader("Authorization") String token) {
+        String userCode = securityUtil.getCurrentUserId(token);
+        ChatRoomResponseDTO responseDTO = chatRoomListService.saveChatRoom(userCode, requestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
-    @GetMapping("/api/chat/rooms/{roomCode}/message")
-    public ResponseEntity<?> getMessages(@RequestHeader("Authorization") String token, @PathVariable int roomCode) {
-        //token에서 userCode 추출
-        String userCode = "1";
-        List<ChatMessagePubDTO> responseDTO = chatMessageService.getMessages(userCode, roomCode);
-        if (responseDTO.isEmpty()) return ResponseEntity.noContent().build();
+    @GetMapping("/room")
+    public ResponseEntity<?> getChatRoomList(@RequestBody ChatRoomListRequestDTO requestDTO, @RequestHeader("Authorization") String token) {
+        String userCode = securityUtil.getCurrentUserId(token);
+        PageResponseDTO responseDTO = chatRoomListService.getChatRoomList(userCode, requestDTO);
+        if (((List<?>) responseDTO.getContent()).isEmpty()) return ResponseEntity.noContent().build();
+        return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+    }
+
+    @GetMapping("/room/{roomCode}")
+    public ResponseEntity<?> getChatMessages(@RequestBody ChatRoomDetailRequestDTO requestDTO, @RequestHeader("Authorization") String token, @PathVariable int roomCode) {
+        String userCode = securityUtil.getCurrentUserId(token);
+        PageResponseDTO responseDTO = chatRoomDetailService.getChatRoomDetail(userCode, requestDTO);
+        if (((ChatRoomDetailResponseDTO) responseDTO.getContent()).getMessages().isEmpty())
+            return ResponseEntity.noContent().build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+
+    @GetMapping("/unread")
+    public ResponseEntity<?> getUnreadMessages(@RequestHeader("Authorization") String token) {
+        String userCode = securityUtil.getCurrentUserId(token);
+        ChatUnreadResponseDTO responseDTO = chatRoomListService.getUnreadMessagesForUser(userCode);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @GetMapping("/{nickname}")
+    public ResponseEntity<?> getChatRoomWithNickname(@RequestHeader("Authorization") String token, @RequestParam String nickname) {
+        String userCode = securityUtil.getCurrentUserId(token);
+        ChatRoomResponseDTO responseDTO = chatRoomListService.getChatRoomByNickname(userCode, nickname);
+        if (responseDTO.getRoomCode() == 0) return ResponseEntity.noContent().build();
         return ResponseEntity.ok(responseDTO);
     }
 }
