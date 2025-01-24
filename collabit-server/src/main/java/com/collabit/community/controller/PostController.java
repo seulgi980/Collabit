6331beeadb0a16cd9a03dcae19aac9765376cf4f
jiseung <kimsz123456@gmail.com>
2025-01-24 14:57:58 +0900

    Fix: PostController 수정 주석 추가 및 API 수정 Related to: #S12P11A605-100

--- collabit-server/src/main/java/com/collabit/community/controller/PostController.java
+++ collabit-server/src/main/java/com/collabit/community/controller/PostController.java
@@ -24,14 +24,16 @@ public class PostController {
     @Operation(summary="게시글 등록",description = "게시글을 등록하는 API입니다.")
     @PostMapping("/")
     public ResponseEntity<?> createPost(@ModelAttribute CreatePostRequestDTO requestDTO){
-        CreatePostResponseDTO responseDTO = postService.createPost("1", requestDTO);
+        String userCode = "1";
+        CreatePostResponseDTO responseDTO = postService.createPost(userCode, requestDTO);
         return ResponseEntity.status(201).body(responseDTO);
     }
 
     @Operation(summary="게시글 목록 조회",description = "게시글 목록을 조회하는 API입니다.")
     @GetMapping("/")
     public ResponseEntity<?> getPostList(){
-        List<GetPostResponseDTO> list = postService.getPostList("1");
+        String userCode = "1";
+        List<GetPostResponseDTO> list = postService.getPostList(userCode);
         if (list.isEmpty()) return ResponseEntity.status(204).build();
         return ResponseEntity.status(200).body(list);
     }
@@ -39,21 +41,24 @@ public class PostController {
     @Operation(summary="게시글 상세 조회",description = "게시글을 상세 조회하는 API입니다.")
     @GetMapping("/{postCode}")
     public ResponseEntity<?> getPost(@PathVariable int postCode){
-        GetPostResponseDTO responseDTO = postService.getPost("1", postCode);
+        String userCode = "1";
+        GetPostResponseDTO responseDTO = postService.getPost(userCode, postCode);
         return ResponseEntity.status(200).body(responseDTO);
     }
 
     @Operation(summary="게시글 수정",description = "게시글을 수정하는 API입니다.")
     @PutMapping("/{postCode}")
     public ResponseEntity<?> updatePost(@PathVariable int postCode,@RequestBody UpdatePostRequestDTO requestDTO){
-        GetPostResponseDTO responseDTO = postService.updatePost("1", postCode, requestDTO);
+        String userCode = "1";
+        GetPostResponseDTO responseDTO = postService.updatePost(userCode, postCode, requestDTO);
         return ResponseEntity.status(201).body(responseDTO);
     }
 
     @Operation(summary="게시글 삭제",description = "게시글을 삭제하는 API입니다.")
     @DeleteMapping("/{postCode}")
     public ResponseEntity<?> deletePost(@PathVariable int postCode){
-        postService.deletePost(postCode);
+        String userCode = "1";
+        postService.deletePost(userCode, postCode);
         return ResponseEntity.status(204).build();
     }
 }
