c2f15b5d4c38f62f29a4e049f412df6a1f3a23d2
lgh9776 <lgh9776@naver.com>
2025-01-25 09:03:32 +0900

    Feat: 상태코드 및 로그인, 회원가입, 연동 시 필요한 데이터 전달
    로그인 시 200, 토큰 발급, 닉네임, 깃허브 아이디 전달
    회원가입 시 201, 토큰 발급, 닉네임, 깃허브 아이디 전달
    연동 시 200, 깃허브 아이디 전달
    
    Resolves: S12P11A605-167

--- collabit-server/src/main/java/com/collabit/oauth/handler/OAuth2SuccessHandler.java
+++ collabit-server/src/main/java/com/collabit/oauth/handler/OAuth2SuccessHandler.java
@@ -2,6 +2,7 @@ package com.collabit.oauth.handler;
 
 import com.collabit.auth.domain.dto.TokenDto;
 import com.collabit.global.security.TokenProvider;
+import com.collabit.oauth.domain.enums.OAuth2Status;
 import com.collabit.oauth.service.OAuth2Service;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,54 +21,55 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private final OAuth2Service oAuth2Service;
     private final TokenProvider tokenProvider;
 
-    // Oauth2 로그인, 회원가입 성공 시 token 발급 및 성공 메시지 전달
-    // 연동 성공 시 메시지만 전달
+    // Oauth2 로그인, 회원가입 성공 시 token 발급 및 상태코드 전달
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
 
-        OAuth2Service.AuthStatus authStatus = oAuth2Service.getAuthStatus(); // 로그인, 회원가입, 연동 구분
+        OAuth2Status oAuth2Status = oAuth2Service.getOAuth2Status(); // 로그인, 회원가입, 연동 구분
         response.setContentType("application/json;charset=UTF-8");
+        JSONObject jsonResponse = new JSONObject();
 
-        String message;
-        switch (authStatus) {
+        switch (oAuth2Status) {
             case GITHUB_LINK_SUCCESS:
                 response.setStatus(HttpServletResponse.SC_OK);
-                message = "GitHub 계정 연동이 완료되었습니다.";
+                jsonResponse.put("githubId", oAuth2Service.getUserGithubId());
                 break;
 
             case LOGIN_SUCCESS:
-                TokenDto loginTokenDto = tokenProvider.generateTokenDto(authentication);
-                addCookie(response, "accessToken", loginTokenDto.getAccessToken(),
-                        tokenProvider.getAccessTokenExpireTime() / 1000);
-                addCookie(response, "refreshToken", loginTokenDto.getRefreshToken(),
-                        tokenProvider.getRefreshTokenExpireTime() / 1000);
                 response.setStatus(HttpServletResponse.SC_OK);
-                message = "로그인이 완료되었습니다.";
+                addAuthTokens(response, authentication);
+                jsonResponse.put("nickname", oAuth2Service.getUserNickname());
+                jsonResponse.put("githubId", oAuth2Service.getUserGithubId());
                 break;
 
             case SIGNUP_SUCCESS:
-                TokenDto signupTokenDto = tokenProvider.generateTokenDto(authentication);
-                addCookie(response, "accessToken", signupTokenDto.getAccessToken(),
-                        tokenProvider.getAccessTokenExpireTime() / 1000);
-                addCookie(response, "refreshToken", signupTokenDto.getRefreshToken(),
-                        tokenProvider.getRefreshTokenExpireTime() / 1000);
                 response.setStatus(HttpServletResponse.SC_CREATED);
-                message = "회원가입이 완료되었습니다.";
+                addAuthTokens(response, authentication);
+                jsonResponse.put("nickname", oAuth2Service.getUserNickname());
+                jsonResponse.put("githubId", oAuth2Service.getUserGithubId());
                 break;
 
             default:
-                throw new IllegalStateException("Unexpected auth status");
+                throw new IllegalStateException("GitHub 인증으로 수행할 수 없습니다.");
         }
 
-        // JSONObject에 데이터를 추가하고 문자열로 변환
-        JSONObject jsonResponse = new JSONObject();
-        jsonResponse.put("message", message);
+        jsonResponse.put("status", response.getStatus());
         response.getWriter().write(jsonResponse.toString());
-
-        oAuth2Service.clearAuthStatus();
     }
 
+    // 인증 정보로부터 토큰을 생성하여 쿠키에 저장
+    private void addAuthTokens(HttpServletResponse response, Authentication authentication) {
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+
+        addCookie(response, "accessToken", tokenDto.getAccessToken(),
+                tokenProvider.getAccessTokenExpireTime() / 1000);
+
+        addCookie(response, "refreshToken", tokenDto.getRefreshToken(),
+                tokenProvider.getRefreshTokenExpireTime() / 1000);
+    }
+
+    // 쿠키 설정(이름, 값, 유효기간, 보안옵션)을 생성하고 응답에 추가
     private void addCookie(HttpServletResponse response, String name, String value, long maxAge) {
         Cookie cookie = new Cookie(name, value);
         cookie.setHttpOnly(true);
