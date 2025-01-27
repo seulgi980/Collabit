c09f6d3429ccef2bfd01968603742195ff0ffeb7
eunseon <jeongisline@gmail.com>
2025-01-27 21:35:35 +0900

    Feat: SwaggerConfig에 Security 설정 추가 JWT 토큰 사용을 위한 스웨거 설정 추가

--- collabit-server/src/main/java/com/collabit/global/config/SwaggerConfig.java
+++ collabit-server/src/main/java/com/collabit/global/config/SwaggerConfig.java
@@ -1,8 +1,10 @@
-package com.collabit.config;
+package com.collabit.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +14,18 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ))
+                .info(new Info()
+                        .title("API Documentation")
+                        .version("1.0.0")
+                        .description("API에 대한 상세 문서입니다.")
+                );
     }
 
     private Info apiInfo() {
