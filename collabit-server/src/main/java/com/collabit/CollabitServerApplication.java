79138c9e192053266628ad9c71c402d22f1533fc
eunseon <jeongisline@gmail.com>
2025-01-27 23:18:04 +0900

    Fix: Application 파일 import문 추가 @EnableJpaAuditing에 대한 Import문 추가

--- collabit-server/src/main/java/com/collabit/CollabitServerApplication.java
+++ collabit-server/src/main/java/com/collabit/CollabitServerApplication.java
@@ -4,6 +4,7 @@ package com.collabit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableCaching
 @SpringBootApplication
