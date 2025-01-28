6ec09945113a1371c17c88ef5ad25e07941e13f4
jiseung <kimsz123456@gmail.com>
2025-01-28 12:22:57 +0900

    Fix: 일반로그인 수정 Authentication 부분에서 userCode로 user불러오는 부분 userEmail로 불러오도록 수정

--- collabit-server/src/main/java/com/collabit/global/security/CustomUserDetailsService.java
+++ collabit-server/src/main/java/com/collabit/global/security/CustomUserDetailsService.java
@@ -14,10 +14,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String userCode) throws UsernameNotFoundException {
-        User user = userRepository.findByCode(userCode)
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(userEmail)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
-
         return new CustomUserDetails(
                 user.getCode(), // PK
                 user.getEmail(),
