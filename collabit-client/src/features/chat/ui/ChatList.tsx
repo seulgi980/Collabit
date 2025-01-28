20f6b441546cdf9eb87afe65d40c8e8f80d9c4c1
clapsheep <sktndid1203@naver.com>
2025-01-28 16:12:38 +0900

    feat : chatInput 수정 - 엔터 및 시프트 엔터 호환 - 빈 문자일 시 그냥 리턴 - input -> textarea로 변경

--- collabit-client/src/features/chat/ui/ChatList.tsx
+++ collabit-client/src/features/chat/ui/ChatList.tsx
@@ -2,37 +2,18 @@
 import ChatListCard, {
   ChatListCardProps,
 } from "@/entities/chat/ui/ChatListCard";
-import { cn } from "@/shared/lib/shadcn/utils";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import TabNavButton from "@/entities/common/ui/TabNavButton";
 
 export default function ChatList({
   chatList,
 }: {
   chatList: ChatListCardProps[];
 }) {
-  const pathname = usePathname();
   return (
     <div className="flex flex-col items-center gap-3 px-2 md:py-4">
       <div className="flex w-full justify-evenly py-3">
-        <Link
-          href="/chat"
-          className={cn(
-            "text-lg font-bold",
-            pathname.includes("/chat") && "border-b-2 border-violet-500",
-          )}
-        >
-          일반
-        </Link>
-        <Link
-          href="/feedback"
-          className={cn(
-            "text-lg font-bold",
-            pathname.includes("/feedback") && "border-b-2 border-violet-500",
-          )}
-        >
-          프로젝트
-        </Link>
+        <TabNavButton href="/chat">일반</TabNavButton>
+        <TabNavButton href="/feedback">프로젝트</TabNavButton>
       </div>
       {/* 채팅 리스트 아이템 */}
       <div className="flex h-[calc(100vh-220px)] w-full flex-col gap-2 overflow-y-auto md:h-[calc(100vh-192px)]">
