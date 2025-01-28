9656d213342b8e4991853cfb34b2e53789a79f44
jiseung <kimsz123456@gmail.com>
2025-01-28 17:19:24 +0900

    Fix: CommentService 수정 User관련 로직 수정, 예외 처리 수정 Related to: #S12P11A605-89

--- collabit-server/src/main/java/com/collabit/community/service/CommentService.java
+++ collabit-server/src/main/java/com/collabit/community/service/CommentService.java
@@ -7,8 +7,14 @@ import com.collabit.community.domain.dto.UpdateCommentRequestDTO;
 import com.collabit.community.domain.entity.Comment;
 import com.collabit.community.domain.entity.Post;
 import com.collabit.community.exception.CommentNotFoundException;
+import com.collabit.community.exception.ParentCommentNotFoundException;
+import com.collabit.community.exception.PostNotFoundException;
 import com.collabit.community.repository.CommentRepository;
 import com.collabit.community.repository.PostRepository;
+import com.collabit.user.domain.entity.User;
+import com.collabit.user.exception.UserDiffrentException;
+import com.collabit.user.exception.UserNotFoundException;
+import com.collabit.user.repository.UserRepository;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +26,7 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
 
     public CreateCommentResponseDTO createComment(CreateCommentRequestDTO requestDTO,
@@ -47,11 +54,10 @@ public class CommentService {
 
     public GetCommentResponseDTO updateComment(String userCode, int commentCode, UpdateCommentRequestDTO requestDTO) {
         // commentCode에 해당하는 comment 조회
-        Comment comment = commentRepository.findByCode(commentCode);
-        // 해당 comment가 없으면 예외 처리
-        if(comment==null) throw new CommentNotFoundException();
+        Comment comment = commentRepository.findByCode(commentCode)
+            .orElseThrow(() -> new CommentNotFoundException());
         // 가지고 온 comment의 userCode와 요청 userCode가 일치하지 않으면 예외 처리
-        if(!comment.getUserCode().equals(userCode)) throw new RuntimeException();
+        if(!comment.getUser().getCode().equals(userCode)) throw new UserDiffrentException();
         // comment의 내용 수정
         comment.setContent(requestDTO.getContent());
         // DB 저장
@@ -63,11 +69,10 @@ public class CommentService {
 
     public void deleteComment(String userCode, int commentCode) {
         // commentCode에 해당하는 comment 조회
-        Comment comment = commentRepository.findByCode(commentCode);
-        // 해당 comment가 없으면 예외 처리
-        if(comment==null) throw new CommentNotFoundException();
+        Comment comment = commentRepository.findByCode(commentCode)
+            .orElseThrow(() -> new CommentNotFoundException());
         // 가지고 온 comment의 userCode와 요청 userCode가 일치하지 않으면 예외 처리
-        if(!comment.getUserCode().equals(userCode)) throw new RuntimeException();
+        if(!comment.getUser().getCode().equals(userCode)) throw new UserDiffrentException();
         // comment 삭제
         comment.setDeleted(true);
         // DB 저장
@@ -76,10 +81,14 @@ public class CommentService {
     
     // Comment 생성 메서드
     public Comment buildComment(CreateCommentRequestDTO requestDTO, int postCode,String userCode) {
-        Post post = postRepository.findByCode(postCode);
-        Comment comment = Comment.builder().content(requestDTO.getContent()).post(post).userCode(userCode).build();
+        User user = userRepository.findByCode(userCode)
+            .orElseThrow(() -> new UserNotFoundException());
+        Post post = postRepository.findByCode(postCode)
+            .orElseThrow(() -> new PostNotFoundException());
+        Comment comment = Comment.builder().content(requestDTO.getContent()).post(post).user(user).build();
         if(requestDTO.getParentCommentCode()!=null){
-            Comment parentComment = commentRepository.findByCode(requestDTO.getParentCommentCode());
+            Comment parentComment = commentRepository.findByCode(requestDTO.getParentCommentCode())
+                .orElseThrow(() -> new ParentCommentNotFoundException());
             comment.setParentComment(parentComment);
         }
         return comment;
@@ -90,7 +99,7 @@ public class CommentService {
         GetCommentResponseDTO responseDTO = GetCommentResponseDTO.builder()
             .code(comment.getCode())
             .postCode(comment.getPost().getCode())
-            .userCode(comment.getUserCode())
+            .userNickname(comment.getUser().getNickname())
             .content(comment.getContent())
             .createdAt(comment.getCreatedAt())
             .updatedAt(comment.getUpdatedAt())
