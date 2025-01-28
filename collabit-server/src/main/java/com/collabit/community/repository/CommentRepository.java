3eb89bfb2914cf4cea3ce005d1162a3d460aceed
jiseung <kimsz123456@gmail.com>
2025-01-28 17:20:52 +0900

    Fix: CommentRepository 수정 User관련 로직 수정, 예외 처리 수정 Related to: #S12P11A605-89

--- collabit-server/src/main/java/com/collabit/community/repository/CommentRepository.java
+++ collabit-server/src/main/java/com/collabit/community/repository/CommentRepository.java
@@ -2,11 +2,12 @@ package com.collabit.community.repository;
 
 import com.collabit.community.domain.entity.Comment;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
-    Comment findByCode(int parentCommentCode);
+    Optional<Comment> findByCode(int parentCommentCode);
 
     List<Comment> findByPostCode(int postCode);
 }
