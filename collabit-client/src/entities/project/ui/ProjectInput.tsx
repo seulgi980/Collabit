21da055c45d46828d80614a9929ac25f1fb1928f
eunseon <jeongisline@gmail.com>
2025-01-29 17:43:02 +0900

    Design: ProjectInput 높이, 너비, 여백 수정

--- collabit-client/src/entities/project/ui/ProjectInput.tsx
+++ collabit-client/src/entities/project/ui/ProjectInput.tsx
@@ -15,12 +15,12 @@ const ProjectInput = ({
 }: ProjectInputProps) => {
   return (
     <form className="flex w-full items-center" onSubmit={handleSearchKeyword}>
-      <span className="z-10 mr-[-30px] flex items-center justify-center">
-        <SearchIcon className="h-full w-full text-gray-400" />
+      <span className="z-10 mr-[-40px] flex h-8 w-8 items-center justify-center">
+        <SearchIcon className="h-5 w-5 text-gray-400" />
       </span>
       <Input
         placeholder="레포지토리명을 입력하세요."
-        className="ml-2 w-full bg-white py-4 pl-[35px] text-xs"
+        className="ml-2 w-full bg-white py-4 pl-[30px] text-xs"
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
       />
