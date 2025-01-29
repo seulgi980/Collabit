41a813777e68b3d1dd930521d6a46c1818486984
eunseon <jeongisline@gmail.com>
2025-01-29 17:55:08 +0900

    Feat: 프로젝트 페이지 프로젝트 추가 함수 작성 projectCreate 데이터에 식별자 추가 handleCreateProject 기능 추가 Related to: S12P11A605-248

--- collabit-client/src/app/(routes)/project/create/page.tsx
+++ collabit-client/src/app/(routes)/project/create/page.tsx
@@ -1,61 +1,126 @@
 "use client";
 
-import ProjectCotnributor from "@/entities/project/ui/ProjectContributor";
 import ProjectHeader from "@/entities/project/ui/ProjectHeader";
 import ProjectInput from "@/entities/project/ui/ProjectInput";
 import ProjectSelect from "@/entities/project/ui/ProjectSelect";
-import ProjectAddCard from "@/features/project/ProjectCreateCard";
+import ProjectCreateCard from "@/features/project/ui/ProjectCreateCard";
+import { ProjectCreate } from "@/shared/types/model/Project";
 import { useState } from "react";
 
 export default function Page() {
   const orgList = ["organization1", "organization2", "organization3"];
 
-  const isList = false;
-
   const [selectType, setSelectType] = useState("");
   const [keyword, setKeyword] = useState("");
 
-  const projects = [
+  {
+    /* TODO: 선택 버튼 눌렀을 때 프로젝트 추가하는 API 연결 필요 */
+  }
+  const handleCreateProject =
+    (project: ProjectCreate) =>
+    (event: React.MouseEvent<HTMLButtonElement>): void => {
+      event.preventDefault();
+      console.log("Creating project", project);
+    };
+
+  const projectCreate = [
     {
+      code: 1,
       title: "프로젝트 A",
-      contributors: [
-        { githubId: "id1", profileImage: "https://github.com/shadcn.png" },
-        { githubId: "id2", profileImage: "https://github.com/shadcn.png" },
-        { githubId: "id3", profileImage: "https://github.com/shadcn.png" },
+      contributor: [
+        {
+          code: 1,
+          githubId: "id1",
+          profileImage: "https://github.com/shadcn.png",
+        },
+        {
+          code: 2,
+          githubId: "id2",
+          profileImage: "https://github.com/shadcn.png",
+        },
+        {
+          code: 3,
+          githubId: "id3",
+          profileImage: "https://github.com/shadcn.png",
+        },
       ],
-      timestamp: new Date("2024-01-28T10:00:00Z"), // Date 객체로 설정
+      timestamp: new Date("2024-01-28T10:00:00Z"),
     },
     {
+      code: 2,
       title: "프로젝트 B",
-      contributors: [
-        { githubId: "id4", profileImage: "https://github.com/shadcn.png" },
-        { githubId: "id5", profileImage: "https://github.com/shadcn.png" },
+      contributor: [
+        {
+          code: 4,
+          githubId: "id4",
+          profileImage: "https://github.com/shadcn.png",
+        },
+        {
+          code: 5,
+          githubId: "id5",
+          profileImage: "https://github.com/shadcn.png",
+        },
       ],
       timestamp: new Date("2024-12-25T14:30:00Z"),
     },
     {
+      code: 3,
       title: "프로젝트 C",
-      contributors: [
-        { githubId: "id6", profileImage: "https://github.com/shadcn.png" },
-        { githubId: "id7", profileImage: "https://github.com/shadcn.png" },
-        { githubId: "id8", profileImage: "https://github.com/shadcn.png" },
-        { githubId: "id9", profileImage: "https://github.com/shadcn.png" },
+      contributor: [
+        {
+          code: 6,
+          githubId: "id6",
+          profileImage: "https://github.com/shadcn.png",
+        },
+        {
+          code: 7,
+          githubId: "id7",
+          profileImage: "https://github.com/shadcn.png",
+        },
+        {
+          code: 8,
+          githubId: "id8",
+          profileImage: "https://github.com/shadcn.png",
+        },
+        {
+          code: 9,
+          githubId: "id9",
+          profileImage: "https://github.com/shadcn.png",
+        },
       ],
       timestamp: new Date("2025-01-20T09:15:00Z"),
     },
     {
+      code: 4,
       title: "프로젝트 D",
-      contributors: [
-        { githubId: "id10", profileImage: "https://github.com/shadcn.png" },
+      contributor: [
+        {
+          code: 10,
+          githubId: "id10",
+          profileImage: "https://github.com/shadcn.png",
+        },
       ],
       timestamp: new Date("2025-01-22T16:45:00Z"),
     },
     {
+      code: 5,
       title: "프로젝트 E",
-      contributors: [
-        { githubId: "id11", profileImage: "https://github.com/shadcn.png" },
-        { githubId: "id12", profileImage: "https://github.com/shadcn.png" },
-        { githubId: "id13", profileImage: "https://github.com/shadcn.png" },
+      contributor: [
+        {
+          code: 11,
+          githubId: "id11",
+          profileImage: "https://github.com/shadcn.png",
+        },
+        {
+          code: 12,
+          githubId: "id12",
+          profileImage: "https://github.com/shadcn.png",
+        },
+        {
+          code: 13,
+          githubId: "id13",
+          profileImage: "https://github.com/shadcn.png",
+        },
       ],
       timestamp: new Date("2025-01-18T08:00:00Z"),
     },
@@ -68,34 +133,31 @@ export default function Page() {
   };
 
   return (
-    <div className="mx-auto flex w-full flex-col justify-center gap-5 py-10 md:w-[580px]">
+    <div className="mx-auto flex w-full flex-col justify-center gap-5 py-10 md:w-[540px]">
       <ProjectHeader
         mainTitle="프로젝트 등록"
         subTitle="프로젝트를 등록하고, 동료들에게 피드백을 요청해보세요."
-        isList={isList}
+        isList={false}
       />
       <div className="flex items-center justify-between gap-2">
-        {!isList && (
-          <ProjectSelect
-            nickname="nickname"
-            organizations={orgList}
-            selectType={selectType}
-            setSelectType={setSelectType}
-          />
-        )}
+        <ProjectSelect
+          nickname="nickname"
+          organizations={orgList}
+          selectType={selectType}
+          setSelectType={setSelectType}
+        />
         <ProjectInput
           keyword={keyword}
           setKeyword={setKeyword}
           handleSearchKeyword={handleSearchKeyword}
         />
       </div>
-      {projects.map((project, index) => {
+      {projectCreate.map((project) => {
         return (
-          <ProjectAddCard
-            key={index}
-            contributors={project.contributors}
-            title={project.title}
-            timestamp={project.timestamp}
+          <ProjectCreateCard
+            key={project.code}
+            project={project}
+            handleCreateProject={handleCreateProject}
           />
         );
       })}
