dd278d526f44e23f0280849265fff11325d0dd46
jiseung <kimsz123456@gmail.com>
2025-01-30 14:15:26 +0900

    Style: 파일 이름 수정 Diffrent -> Different로 수정

--- collabit-server/src/main/java/com/collabit/user/exception/UserDiffrentException.java
+++
@@ -1,8 +0,0 @@
-package com.collabit.user.exception;
-
-public class UserDiffrentException extends RuntimeException {
-
-    public UserDiffrentException() {
-        super("유저 정보가 일치하지 않습니다.");
-    }
-}
