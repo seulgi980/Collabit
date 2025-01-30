8b03d673dc38e9216b74514ae9e70b952b232f33
clapsheep <sktndid1203@naver.com>
2025-01-31 00:19:43 +0900

    feat :커뮤니티 post 기능 구현 - 반응형에 따른 크기 조절 - 이미지 4개 까지 업로드 가능 - 이미지 프리뷰 캐러셀 구현 - 이미지 제거 추가 기능 구현 related #S12P11A605-284

--- collabit-client/src/app/(routes)/community/page.tsx
+++ collabit-client/src/app/(routes)/community/page.tsx
@@ -1,3 +1,52 @@
+"use client";
+import usePost from "@/features/community/api/usePost";
+import Post from "@/features/community/ui/Post";
+
 export default function Page() {
-  return <div>Community</div>;
+  const {
+    images,
+    preview,
+    content,
+    handleImageChange,
+    handleContentChange,
+    handleDeleteImage,
+    handleSubmit,
+  } = usePost();
+
+  return (
+    <div className="mx-auto max-w-5xl py-5 md:py-10">
+      <h2 className="sr-only">커뮤니티</h2>
+      <Post
+        images={images}
+        preview={preview}
+        content={content}
+        handleImageChange={handleImageChange}
+        handleContentChange={handleContentChange}
+        handleDeleteImage={handleDeleteImage}
+        handleSubmit={handleSubmit}
+      />
+      {/* <ul
+          className={cn(
+            preview.length > 0 &&
+              "grid h-[260px] w-[400px] overflow-hidden rounded-lg",
+            preview.length === 1 && "grid-cols-1",
+            preview.length === 2 && "grid-cols-2",
+            preview.length === 3 && "grid-cols-2",
+            preview.length === 4 && "grid-cols-2 grid-rows-2",
+          )}
+        >
+          {preview.map((preview, index) => (
+            <li key={preview} className="relative h-full w-full">
+              <Image
+                className="object-cover"
+                src={preview}
+                alt={`미리보기 ${index + 1}`}
+                fill
+                sizes="(max-width: 400px) 100vw"
+              />
+            </li>
+          ))}
+        </ul> */}
+    </div>
+  );
 }
