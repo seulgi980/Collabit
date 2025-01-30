e47ac4342f55941a64729c23ab7f1937e3d7cce2
lgh9776 <lgh9776@naver.com>
2025-01-31 00:46:47 +0900

    Feat: Project 관련 엔티티 구현
    Project, ProjectInfo, Contributor, ProjectContributor 엔티티 구현
    ProjectContributorId 클래스로 복합키 설정
    
    Resolves: S12P11A605-287

---
+++ collabit-server/src/main/java/com/collabit/project/domain/entity/Project.java
@@ -0,0 +1,30 @@
+package com.collabit.project.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int code;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "organization")
+    private String organizationName;
+
+    @OneToMany(mappedBy = "project")
+    private List<ProjectInfo> projectInfos;
+
+    @OneToMany(mappedBy = "project")
+    private List<ProjectContributor> projectContributors;
+}
