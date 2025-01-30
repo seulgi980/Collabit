e067ebb188c6cc98645e31fd1af4be6346b07dd7
jiseung <kimsz123456@gmail.com>
2025-01-30 14:16:33 +0900

    Fix: CommentRepository 수정 @Repository 어노테이션 추가

--- collabit-server/src/main/java/com/collabit/community/repository/CommentRepository.java
+++ collabit-server/src/main/java/com/collabit/community/repository/CommentRepository.java
@@ -4,7 +4,9 @@ import com.collabit.community.domain.entity.Comment;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
     Optional<Comment> findByCode(int parentCommentCode);
