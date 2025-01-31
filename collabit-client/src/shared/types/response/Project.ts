33a9bafde51b72695adb3dd87d50e88fdd2d60d2
eunseon <jeongisline@gmail.com>
2025-01-31 15:56:38 +0900

    Feat: Github API 연동을 위한 Response type 추가 Related to: S12P11A605-250

--- collabit-client/src/shared/types/response/Project.ts
+++ collabit-client/src/shared/types/response/Project.ts
@@ -1,3 +1,15 @@
+import { Contributor, Project } from "../model/Project";
+
 export type ProjectUpdateCheckResponse = {
   isUpdated: boolean;
+};
+
+export type GithubRepoResponse = Pick<Project, "title" | "organization"> & {
+  updatedAt: Date;
+};
+
+export type GithubOrgResponse = Pick<Project, "organization">;
+
+export type GithubCollaboratorResponse = {
+  contributors: Contributor[];
 };
