d4e61878fb00a7abf7914d3d8b24ee5e440ba29d
eunseon <jeongisline@gmail.com>
2025-01-31 15:03:36 +0900

    Feat: Project 관련 type, Page 타입입 정의 ERD 타입, request, response 타입 정의 Page 타입 정의 Related to: S12P11A605-290

--- collabit-client/src/shared/types/model/Project.ts
+++ collabit-client/src/shared/types/model/Project.ts
@@ -1,21 +1,29 @@
-export interface ProjectCreate {
+export interface Project {
   code: number;
   title: string;
-  contributor: Contributor[];
-  timestamp: Date;
+  organization: string;
 }
 export interface ProjectInfo {
   code: number;
-  title: string;
+  projectCode: number;
+  userCode: string;
   total: number;
   participant: number;
   isDone: boolean;
-  surveyUrl: string;
-  contributor: Contributor[];
+  sympathy: number;
+  listening: number;
+  expression: number;
+  problemSolving: number;
+  conflictResolution: number;
+  leadership: number;
 }
-
 export interface Contributor {
   code: number;
   githubId: string;
   profileImage: string;
 }
+export type ProjectListItem = Pick<Project, "title" | "organization"> &
+  Pick<ProjectInfo, "code" | "total" | "participant" | "isDone"> & {
+    contributors: Contributor[];
+    isUpdated: boolean;
+  };
---
+++ collabit-client/src/shared/types/response/Project.ts
@@ -0,0 +1,3 @@
+export type ProjectUpdateCheckResponse = {
+  isUpdated: boolean;
+};
