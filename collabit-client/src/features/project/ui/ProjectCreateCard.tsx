7b65347a52bacd5c30f6e4eb7939e6e3598be2e7
eunseon <jeongisline@gmail.com>
2025-01-31 18:20:43 +0900

    Feat: ProjectCreateCard Props 수정 github api를 프론트에서 직접 호출하기 위해 props 변경 Related to: S12P11A605-250

--- collabit-client/src/features/project/ui/ProjectCreateCard.tsx
+++ collabit-client/src/features/project/ui/ProjectCreateCard.tsx
@@ -1,16 +1,22 @@
 import ProjectCotnributor from "@/entities/project/ui/ProjectContributor";
-import { ProjectCreate } from "@/shared/types/model/Project";
 import { Button } from "@/shared/ui/button";
 import { Card, CardDescription, CardTitle } from "@/shared/ui/card";
 import { LockIcon } from "lucide-react";
+import { useProjectCreate } from "../api/useProjectCreate";
+import {
+  GithubCollaboratorResponse,
+  GithubRepoResponse,
+} from "@/shared/types/response/Project";
 
 interface ProjectCreateCardProps {
-  project: ProjectCreate;
+  project: GithubRepoResponse & GithubCollaboratorResponse;
 }
 
 const ProjectCreateCard = ({ project }: ProjectCreateCardProps) => {
+  const { handleCreateProject } = useProjectCreate();
+
   const now = new Date();
-  const diffInMilliseconds = now.getTime() - project.timestamp.getTime();
+  const diffInMilliseconds = now.getTime() - project.updatedAt.getTime();
 
   const hours = Math.floor(diffInMilliseconds / 3600000);
   const days = Math.floor(hours / 24);
@@ -28,18 +34,10 @@ const ProjectCreateCard = ({ project }: ProjectCreateCardProps) => {
   } else if (hours > 0) {
     timeAgo = `${hours}시간 전`;
   }
-
-  {
-    /* TODO: 선택 버튼 눌렀을 때 프로젝트 추가하는 API 연결 필요 */
-  }
-  const handleCreateProject = (project: ProjectCreate) => {
-    console.log("Creating project", project);
-  };
-
   return (
     <Card className="flex h-[90px] items-center justify-between bg-violet-50 px-4 drop-shadow-lg">
       <div className="flex flex-row items-center justify-between gap-4">
-        <ProjectCotnributor contributor={project.contributor} />
+        <ProjectCotnributor contributor={project.contributors} />
         <CardTitle className="text-lg md:text-xl">{project.title}</CardTitle>
       </div>
       <div className="flex items-center justify-between">
