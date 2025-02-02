ace41ddef8e152b8dd416ed80090d4b8ac5a67f3
eunseon <jeongisline@gmail.com>
2025-02-02 15:02:55 +0900

    Fix: Project Request 타입 파일명 대소문자 수정

--- collabit-client/src/shared/types/request/project.ts
+++
@@ -1,7 +0,0 @@
-import { Contributor, Project, ProjectInfo } from "../model/Project";
-
-export type ProjectCreateRequest = Pick<Project, "title" | "organization"> & {
-  contributors: Contributor[];
-};
-export type ProjectRemoveRequest = Pick<ProjectInfo, "code">;
-export type ProjectFinishRequest = Pick<ProjectInfo, "code">;
