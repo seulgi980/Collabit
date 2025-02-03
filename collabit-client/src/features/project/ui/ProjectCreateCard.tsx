d992abcf859e605850e7f3daccc997f29dc01d14
eunseon <jeongisline@gmail.com>
2025-02-03 19:21:50 +0900

    Design: ProjectCreateCard 디자인 수정 Related to: S12P11A605-248

--- collabit-client/src/features/project/ui/ProjectCreateCard.tsx
+++ collabit-client/src/features/project/ui/ProjectCreateCard.tsx
@@ -1,58 +1,46 @@
-import ProjectContributor from "@/entities/project/ui/ProjectContributor";
-import { Button } from "@/shared/ui/button";
 import { Card, CardDescription, CardTitle } from "@/shared/ui/card";
-import { LockIcon } from "lucide-react";
 import { FormattedGithubRepo } from "@/shared/types/response/github";
-import { useCreateProject } from "../api/useCreateProject";
-import { useGithubContributors } from "../api/useGithubContributors";
 import formatRelativeTime from "@/shared/utils/formatRelativeTime";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import ProjectCreateButton from "@/entities/project/ui/ProjectCreateButton";
 
 interface ProjectCreateCardProps {
   project: FormattedGithubRepo;
+  isSelected?: boolean;
 }
 
-const ProjectCreateCard = ({ project }: ProjectCreateCardProps) => {
-  const { handleCreateProject, isAdded } = useCreateProject(
-    project.organization,
-    project.title,
-  );
-
-  const { contributors, isLoading: isContributorsLoading } =
-    useGithubContributors(project.organization, project.title);
-
-  const timeAgo = formatRelativeTime(project.updatedAt);
+const ProjectCreateCard = ({ project, isSelected }: ProjectCreateCardProps) => {
+  const router = useRouter();
+  const timeAgo = formatRelativeTime(project.updated_at);
 
   return (
-    <Card className="flex h-[90px] items-center justify-between bg-violet-50 px-4 drop-shadow-lg">
+    <Card
+      className={cn(
+        "flex h-[60px] cursor-pointer items-center justify-between px-4 transition-colors",
+        isSelected
+          ? "bg-violet-200 hover:bg-violet-300"
+          : "bg-violet-50 hover:bg-violet-100",
+      )}
+      onClick={() => {
+        router.push(`/project/create?repo=${project.title}`);
+      }}
+    >
       <div className="flex flex-row items-center justify-between gap-4">
-        {isContributorsLoading ? (
-          <div className="h-8 w-8 animate-pulse rounded-full bg-gray-200" />
-        ) : (
-          <ProjectContributor contributor={contributors || []} />
-        )}
-        <CardTitle className="text-lg md:text-xl">{project.title}</CardTitle>
+        <Image
+          width={24}
+          height={24}
+          src="/images/github-profile.png"
+          alt="Github Profile"
+        />
+        <CardTitle className="text-md md:text-lg">{project.title}</CardTitle>
       </div>
       <div className="flex items-center justify-between gap-2">
-        {isAdded && (
-          <Button variant="ghost" className="h-4 w-4">
-            <LockIcon className="h-full w-full text-gray-400" />
-          </Button>
-        )}
-        <CardDescription className="mr-4 hidden w-[50px] text-right text-gray-400 md:block">
+        <CardDescription className="mr-4 hidden w-[100px] text-right text-gray-500 md:block">
           {timeAgo}
         </CardDescription>
-        <Button
-          className="bg-black"
-          onClick={() =>
-            handleCreateProject({
-              ...project,
-              contributors: contributors || [],
-            })
-          }
-          disabled={isAdded}
-        >
-          {isAdded ? "추가됨" : "선택"}
-        </Button>
+        <ProjectCreateButton project={project} />
       </div>
     </Card>
   );
