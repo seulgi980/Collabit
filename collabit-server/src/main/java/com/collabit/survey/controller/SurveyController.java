858f8c20c0d5b112e07395e93c2ec41e8ee27cf4
eunseon <jeongisline@gmail.com>
2025-02-04 15:33:03 +0900

    Feat: SurveyController 작성 설문 참여여부 확인 API를 설문리스트 조회 API로 확장 객관식 설문 답변 저장 API mapping 수정 객관식, 주관식 설문 답변 조회 API 개발 Related to: S12P11A605-327, S12P11A605-329, S12P11A605-305, S12P11A605-306, S12P11A605-307

--- collabit-server/src/main/java/com/collabit/survey/controller/SurveyController.java
+++ collabit-server/src/main/java/com/collabit/survey/controller/SurveyController.java
@@ -1,13 +1,9 @@
 package com.collabit.survey.controller;
 
-import com.collabit.survey.domain.dto.ApiTextResponseDTO;
-import com.collabit.survey.domain.dto.SurveyQuestionResponseDTO;
-import com.collabit.survey.domain.dto.SurveyResponseSaveRequestDTO;
-import com.collabit.survey.domain.entity.SurveyResponse;
+import com.collabit.survey.domain.dto.*;
 import com.collabit.survey.service.SurveyService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,31 +20,44 @@ import java.util.List;
 public class SurveyController {
     private final SurveyService surveyService;
 
-    @GetMapping("/check")
-    @Operation(summary="특정 userCode 유저가 특정 projectInfoCode 의 설문에 참여했는지 확인", description = "A가 projectInfoCode 1번의 설문을 이미 완료했는지 확인하는 API 입니다.(true면 참여완료)")
-    public ResponseEntity<Boolean> checkIfUserCanEvaluate(
-            @RequestParam int projectInfoCode, HttpServletRequest request) {
-        boolean canEvaluate = surveyService.canUserEvaluate(projectInfoCode, request);
-        return ResponseEntity.ok(!canEvaluate);
+    @GetMapping
+    @Operation(summary="설문 리스트 조회", description = "참여 전/후의 설문 리스트를 조회하는 API입니다.")
+    public ResponseEntity<?> getSurveyList() {
+        List<SurveyListResponseDTO> responseDTO = surveyService.getSurveyList();
+        if (responseDTO.isEmpty()) return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(responseDTO);
     }
 
-    @GetMapping("/questions")
+    @GetMapping("/question")
     @Operation(summary="객관식 질문 24개 조회", description = "객관식 설문 질문 문항 전체 24개를 조회하는 API 입니다.")
     public ResponseEntity<List<SurveyQuestionResponseDTO>> findAllQuestions() {
         return ResponseEntity.ok(surveyService.getAllQuestions());
     }
 
-    @PostMapping("/response/save")
-    @Operation(summary="객관식 설문 답변 저장하기", description = "24개의 객관식 설문 답변내용을 mongoDB에 저장하는 API 입니다.")
-    public ResponseEntity<ApiTextResponseDTO> saveResponse(
-            @RequestParam int projectInfoCode,
-            @RequestBody @Valid SurveyResponseSaveRequestDTO surveyResponseDTO,
-            HttpServletRequest request) {
+    @PostMapping("/response/{surveyCode}/multiple")
+    @Operation(summary="객관식 설문 답변 저장", description = "24개의 객관식 설문 답변내용을 mongoDB에 저장하는 API 입니다.")
+    public ResponseEntity<ApiTextResponseDTO> saveMultipleResponse(
+            @PathVariable int surveyCode,
+            @RequestBody @Valid SurveyMultipleRequestDTO requestDTO) {
 
-        List<Integer> scores = surveyResponseDTO.getScores();
-        SurveyResponse surveyResponse = surveyService.saveResponse(projectInfoCode, scores, request);
-        log.debug("surveyResponse: {}", surveyResponse);
+        List<Integer> scores = requestDTO.getScores();
+        surveyService.saveResponse(surveyCode, scores);
         return ResponseEntity.ok(new ApiTextResponseDTO("설문 응답이 성공적으로 저장되었습니다."));
     }
 
+    @GetMapping("/response/{surveyCode}/multiple")
+    @Operation(summary="객관식 설문 답변 조회", description = "24개의 객관식 설문 답변내용을 조회하는 API입니다.")
+    public ResponseEntity<SurveyMultipleResponseDTO> getMultipleResponse(@PathVariable int surveyCode) {
+        SurveyMultipleResponseDTO responseDTO = surveyService.getMultipleResponse(surveyCode);
+        log.debug("surveyResponse: {}", responseDTO);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @GetMapping("/response/{surveyCode}/essay")
+    @Operation(summary="주관식 설문 답변 조회", description = "주관식 설문 답변내용을 조회하는 API입니다.")
+    public ResponseEntity<SurveyEssayResponseDTO> getEssayResponse(@PathVariable int surveyCode) {
+        SurveyEssayResponseDTO responseDTO = surveyService.getEssayResponse(surveyCode);
+        log.debug("surveyResponse: {}", responseDTO);
+        return ResponseEntity.ok(responseDTO);
+    }
 }
