357a3e19644fe87a547c092904cba46ae4ff1cd7
eunseon <jeongisline@gmail.com>
2025-02-04 15:39:15 +0900

    Feat: 객관식, 주관식 설문 레포지토리 작성 설문 결과 조회 메서드 Related to: S12P11A605-327, S12P11A605-329

---
+++ collabit-server/src/main/java/com/collabit/survey/repository/SurveyMultipleRepository.java
@@ -0,0 +1,12 @@
+package com.collabit.survey.repository;
+
+import com.collabit.survey.domain.entity.SurveyMultiple;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SurveyMultipleRepository extends MongoRepository<SurveyMultiple, String> {
+    SurveyMultiple findByProjectInfoCodeAndUserCode(int projectInfoCode, String userCode);
+}
