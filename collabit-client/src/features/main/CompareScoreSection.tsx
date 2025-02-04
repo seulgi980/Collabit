8cdfd4daad70a65ea40aa0b85538dd0a5cadb748
clapsheep <sktndid1203@naver.com>
2025-02-04 13:03:19 +0900

    Feat : 메인페이지 비교차트 추가 - chartjs, 리액트 관련 라이브러리 설치 - 전체 평균 협업점수와, 나의 협업점수 비교 차트 구현

--- collabit-client/src/features/main/CompareScoreSection.tsx
+++ collabit-client/src/features/main/CompareScoreSection.tsx
@@ -2,11 +2,12 @@
 
 import { Button } from "@/shared/ui/button";
 import { useAuth } from "../auth/api/useAuth";
+import CompareSection from "../report/ui/CompareSection";
 
 const CompareScoreSection = () => {
   const { userInfo } = useAuth();
   return (
-    <section className="flex w-full flex-wrap items-center gap-4 border-b-2 border-gray-200 pb-10 sm:flex-nowrap md:px-14">
+    <section className="flex w-full flex-wrap items-center justify-center gap-4 border-b-2 border-gray-200 pb-10 sm:flex-nowrap md:px-14">
       <h3 className="sr-only">사용자 평균 협업 점수</h3>
       <div className="flex h-full w-full flex-col items-center justify-center gap-4 md:items-start md:gap-8">
         <span className="text-center text-2xl font-bold sm:text-3xl md:text-left">
@@ -21,8 +22,8 @@ const CompareScoreSection = () => {
           피드백 요청하기
         </Button>
       </div>
-      <div className="flex h-[300px] w-full items-center justify-center rounded-md bg-gray-200">
-        임시 데이터
+      <div className="flex h-[300px] w-full max-w-[600px] items-center justify-center rounded-md bg-gray-50 p-4">
+        <CompareSection />
       </div>
     </section>
   );
