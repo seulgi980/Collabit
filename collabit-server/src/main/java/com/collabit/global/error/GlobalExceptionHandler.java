9123cf445a3b1fc421685110d010ce67a539287b
lgh9776 <lgh9776@naver.com>
2025-02-04 12:26:28 +0900

    Feat: 메소드 파라미터 불일치 예외 처리
    MethodArgumentTypeMismatchException 처리 로직 추가
    
    Related to: S12P11A605-261

--- collabit-server/src/main/java/com/collabit/global/error/GlobalExceptionHandler.java
+++ collabit-server/src/main/java/com/collabit/global/error/GlobalExceptionHandler.java
@@ -1,13 +1,27 @@
 package com.collabit.global.error;
 
+import com.collabit.global.common.ErrorCode;
+import com.collabit.global.common.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * 메소드 파라미터 불일치 예외 처리
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException e) {
+        log.error("MethodArgumentTypeMismatchException: {}", e.getMessage());
 
+        ErrorResponse response = new ErrorResponse(ErrorCode.INVALID_TYPE_VALUE, ErrorCode.INVALID_TYPE_VALUE.getMessage());
 
-
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
 }
