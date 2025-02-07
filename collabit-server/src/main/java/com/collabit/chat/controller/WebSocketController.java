e846f79e0846990ac71a61d4cb607884505b1c99
eunseon <jeongisline@gmail.com>
2025-02-07 20:34:53 +0900

    Reefactor: 웹소켓 인증 설정 SimpMessageHeaderAccessor를 통해 사용자 인증 Related to: S12P11A605-93

--- collabit-server/src/main/java/com/collabit/chat/controller/WebSocketController.java
+++ collabit-server/src/main/java/com/collabit/chat/controller/WebSocketController.java
@@ -5,13 +5,20 @@ import com.collabit.chat.domain.dto.WebSocketMessageDTO;
 import com.collabit.chat.exception.MessageContentEmptyException;
 import com.collabit.chat.service.ChatRedisService;
 import com.collabit.chat.service.WebSocketService;
+import com.collabit.global.security.CustomUserDetails;
 import com.collabit.global.security.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.socket.WebSocketSession;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,9 +30,9 @@ public class WebSocketController {
 
     // 연결 설정
     @MessageMapping("/chat.connect/{roomCode}")
-    public void connectToRoom(@PathVariable int roomCode) {
+    public void connectToRoom(@PathVariable int roomCode, SimpMessageHeaderAccessor headerAccessor) {
         log.debug("Connecting to room {}", roomCode);
-        String userCode = SecurityUtil.getCurrentUserCode();
+        String userCode = getUserCodeFromHeader(headerAccessor);
         // WebSocket 연결 및 상태 업데이트
         webSocketService.handleUserEnter(roomCode, userCode);
         chatRedisService.updateUserStatus(userCode, true);
@@ -33,9 +40,10 @@ public class WebSocketController {
 
     // 메시지 전송
     @MessageMapping("/chat.message/{roomCode}")
-    public void sendMessageToRoom(WebSocketMessageDTO message) {
+    public void sendMessageToRoom(WebSocketMessageDTO message, SimpMessageHeaderAccessor headerAccessor) {
         log.debug("Sending message to room {}", message.toString());
-        String userCode = SecurityUtil.getCurrentUserCode();
+        String userCode = getUserCodeFromHeader(headerAccessor);
+
         // 메시지 유효성 체크
         if (message == null || message.getMessage() == null) {
             log.debug("Message is null or empty");
@@ -47,9 +55,10 @@ public class WebSocketController {
 
     // 연결 해제
     @MessageMapping("/chat.disconnect/{roomCode}")
-    public void disconnect(WebSocketSession session) {
+    public void disconnect(WebSocketSession session, SimpMessageHeaderAccessor headerAccessor) {
         log.debug("Disconnecting from room {}", session.getId());
-        String userCode = SecurityUtil.getCurrentUserCode();
+        String userCode = getUserCodeFromHeader(headerAccessor);
+
         // WebSocket 세션에서 roomCode를 가져오기
         int roomCode = (Integer) session.getAttributes().get("roomCode");
         // 사용자 연결 해제 및 상태 업데이트
@@ -59,9 +68,19 @@ public class WebSocketController {
 
     // 채팅방 이동
     @MessageMapping("/chat.switchRoom")
-    public void switchRoom(ChatRoomSwitchDTO switchDTO) {
+    public void switchRoom(ChatRoomSwitchDTO switchDTO, SimpMessageHeaderAccessor headerAccessor) {
         log.debug("ChatRoomSwitchDTO {}", switchDTO.toString());
-        String userCode = SecurityUtil.getCurrentUserCode();
+        String userCode = getUserCodeFromHeader(headerAccessor);
         webSocketService.switchUserRoom(userCode, switchDTO);
     }
+
+    private String getUserCodeFromHeader(SimpMessageHeaderAccessor headerAccessor) {
+        if (headerAccessor.getUser() instanceof Authentication auth) {
+            if (auth.getPrincipal() instanceof CustomUserDetails userDetails) {
+                return userDetails.getCode();
+            }
+        }
+        throw new RuntimeException("인증되지 않은 사용자입니다.");
+    }
+
 }
