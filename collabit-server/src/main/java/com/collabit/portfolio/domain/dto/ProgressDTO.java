6388e9f2bf264dcdc1427bda2492adc40ff1f267
seulgi980 <seulgi980@naver.com>
2025-02-10 14:56:05 +0900

    Feat: 클래스 구조, 클래스명 변경

--- collabit-server/src/main/java/com/collabit/portfolio/domain/dto/ProgressDTO.java
+++ collabit-server/src/main/java/com/collabit/portfolio/domain/dto/ProgressDTO.java
@@ -2,14 +2,17 @@ package com.collabit.portfolio.domain.dto;
 
 import lombok.*;
 
+import java.util.List;
+
 @AllArgsConstructor
 @Builder
 @ToString
 @NoArgsConstructor
 @Getter
-public class MultipleAverageDTO {
-    private String name;
-    private Double score;
+public class ProgressDTO {
+    private List<ProgressDataDTO> scores;
+    private int min;
+    private int max;
 }
 
 
