4aca19051e4f844271911c10ce89ea8a12a4c311
jiseung <kimsz123456@gmail.com>
2025-02-14 10:56:14 +0900

    Fix: 포트폴리오 상태 조회 로직 수정

--- collabit-server/src/main/java/com/collabit/portfolio/service/PortfolioService.java
+++ collabit-server/src/main/java/com/collabit/portfolio/service/PortfolioService.java
@@ -301,7 +301,7 @@ public class PortfolioService {
             isUpdate = true;
         }
         // 포트폴리오가 이미 생성되었다면 포트폴리오 테이블의 isUpdate도 확인
-        else if(portfolio != null && portfolio.getIsUpdate() && participant >= MIN_PEOPLE_COUNT){
+        else if(portfolio != null && portfolio.getIsUpdate() && participant >= MIN_PEOPLE_COUNT + portfolio.getParticipant()){
             isUpdate = true;
         }
         return isUpdate;
