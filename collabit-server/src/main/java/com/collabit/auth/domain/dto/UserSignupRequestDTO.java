2e6df2ba77d0460117936fe74ad2382099a9c39e
seulgi980 <seulgi980@naver.com>
2025-02-17 16:29:40 +0900

    Feat: 닉네임 2~8자로 비밀번호 8~16자로 제한

--- collabit-server/src/main/java/com/collabit/auth/domain/dto/UserSignupRequestDTO.java
+++ collabit-server/src/main/java/com/collabit/auth/domain/dto/UserSignupRequestDTO.java
@@ -18,8 +18,8 @@ public class UserSignupRequestDTO {
 
 
     @NotBlank(message = "닉네임을 입력해주세요")
-    @Pattern(regexp = "^.{2,16}$",
-    message = "닉네임은 2자 이상 16자 이하로 입력해주세요")
+    @Pattern(regexp = "^.{2,8}$",
+    message = "닉네임은 2자 이상 8자 이하로 입력해주세요")
     private String nickname;
 
 
