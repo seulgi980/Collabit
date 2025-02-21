4e7ceea8af1377bc10bafc8de61f0d5b1660d6c7
lgh9776 <lgh9776@naver.com>
2025-02-21 09:56:55 +0900

    Feat: 총 프로젝트 수 반환 추가

--- collabit-server/src/main/java/com/collabit/portfolio/domain/dto/GetPortfolioStatusResponseDTO.java
+++ collabit-server/src/main/java/com/collabit/portfolio/domain/dto/GetPortfolioStatusResponseDTO.java
@@ -11,4 +11,5 @@ public class GetPortfolioStatusResponseDTO {
     private boolean isUpdate; // 갱신 가능 여부
     private boolean isExist; // 포트폴리오 존재 여부
     private int totalParticipant; // 포트폴리오에 참여한 총 참여자 수
+    private int totalProject; //프로젝트 수
 }
